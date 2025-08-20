b5de9d36bc93fdcc6da2a3f4039d26d6524dbd73
Paco Hobi <pacohobi@me.com>
2025-08-20 19:50:30 +0200

    Fix image uploading to kappa.lol
    We need to include the content type otherwise the hosted file will not have the correct type.

--- utils/images.py
+++ utils/images.py
@@ -11,7 +11,7 @@ async def upload_image(image_url: str) -> str:
     image_data = await res.content.read()
 
     data = aiohttp.FormData()
-    data.add_field('file', image_data)
+    data.add_field('file', image_data, content_type=res.content_type)
 
     res = await requests.session.post(KAPPA_URL, data=data)
     res.raise_for_status()
